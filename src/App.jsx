646661cc4fca1695e0e82f48493496e4e1afcbc6
salob <sianob79@gmail.com>
2025-05-03 19:58:38 +0100

    add new title

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,7 @@ export default function App() {
   return (
     <div className="container">
       <h1>Blood Pressure Calculator</h1>
+      <h3>THIS IS A CANARY</h3>
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="systolic">Systolic Pressure</label>
